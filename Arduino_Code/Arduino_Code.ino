3e5761a0cbcd852ad4e25eb49f3c96df6b4abe35
tedfmyers <tedfmyers@gmail.com>
2016-10-01 19:47:21 -0400

    Arduino file and skeleton code

---
+++ Arduino_Code/Arduino_Code.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Receive data from weather API
+
+  // Determine whether to display current or future weather data
+
+  // Determine weather states
+
+  // Perform actions based on these weather states
+
+}
