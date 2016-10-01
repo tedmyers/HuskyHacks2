86d30721660ba7ddf4cc49c86feda9609036e469
tedfmyers <tedfmyers@gmail.com>
2016-10-01 19:48:58 -0400

    Python file added with minimal skeleton code

---
+++ Python_Code.py
@@ -0,0 +1,6 @@
+'''
+Python code will go here
+1. Poll wunderground API
+2. Parse this data for relevant information (current and tomorrow's weather)
+3. Send this address over a serial FTDI connection to Arduino
+'''
