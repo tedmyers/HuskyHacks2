a1e2c61cceb057d03d8256e750517fcea79d0fa6
tedfmyers <tedfmyers@gmail.com>
2016-10-01 22:02:52 -0400

    very basic python program

--- Python_Code.py
+++ Python_Code.py
@@ -4,3 +4,14 @@ Python code will go here
 2. Parse this data for relevant information (current and tomorrow's weather)
 3. Send this address over a serial FTDI connection to Arduino
 '''
+
+import os
+
+location = 'Boston MA'
+API_key = "09a921928db449a7"
+command = "pywu fetch %s %s" % (location, API_key)
+os.system(command)
+
+f = os.popen('pywu current condition')
+current_condition = f.read()
+print current_condition,
